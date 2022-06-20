fe5a4e388856f0b28490b979a80308e4c737d117
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-06-20 14:51:35 -0300

    test deploy

--- config/database.js
+++ config/database.js
@@ -1,11 +1,22 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
+  defaultConnection: "default",
   connection: {
-    client: 'sqlite',
+    client: "sqlite",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+      },
     },
-    useNullAsDefault: true,
+    options: {
+      ssl: env.bool("DATABASE_SSL", false),
+      useNullAsDefault: true,
+    },
   },
 });
