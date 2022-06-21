f1a8612a20d83306cf1656841b2c9cc70a463320
“gabrielcaiana” <gabrielcaianaguedes@gmail.com>
2022-06-20 22:47:06 -0300

    change database config

--- config/database.js
+++ config/database.js
@@ -1,7 +1,6 @@
 const path = require("path");
 
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
   connection: {
     client: "sqlite",
     connection: {
@@ -10,13 +9,7 @@ module.exports = ({ env }) => ({
         "..",
         env("DATABASE_FILENAME", ".tmp/data.db")
       ),
-      ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
-      },
     },
-    options: {
-      ssl: env.bool("DATABASE_SSL", false),
-      useNullAsDefault: true,
-    },
+    useNullAsDefault: true,
   },
 });
